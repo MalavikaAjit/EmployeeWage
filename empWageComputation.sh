0bd32d52be205d7872efaa0dcaf7d6165ada8802
MalavikaAjit <malavikaajit9@gmail.com>
2021-02-24 22:46:43 +0530

    REFACTOR. added wages of a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,44 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
-isPresent=1;
-checkRandom=$((RANDOM%2))
-if [ $isPresent -eq $checkRandom ]
-then
-    echo "Employee is Present"
-else
-    echo "Employee is absent"
-fi
+present=1
+absent=0
+FullTime=1
+PartTime=0
+WagePerHr=20
+fullDay=8
+partTimeHr=4
+dailyPayment=0
+workingDay=20
+salary=0
+
+for((day=1; day<=$workingDay; day++))
+do
+    checkRandom=$((RANDOM%2))
+    case $checkRandom in
+	    $present)
+    		    echo "Employee is present"
+    		    jobType=$((RANDOM%2))
+    		    case $jobType in
+                        $FullTime)
+                            dailyPayment=$(($WagePerHr * $fullDay))
+                            ;;
+                        $PartTime)
+                            dailyPayment=$(($WagePerHr * $partTimeHr))
+                            ;;
+                        *)
+                            echo "Invalid job type"
+                            ;;
+                esac
+                ;;
+	    $absent)
+    		    echo "Employee is absent"
+			    ;;
+        *)
+                echo "Invalid"
+                ;;
+        esac
+        salary=$(($dailyPayment+$salary))
+done
+
+echo "Montly Salary is $salary"
